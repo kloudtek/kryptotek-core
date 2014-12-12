db67fa7565c885a02b613f4ca17860178a085f9f
Yannick Menager <ymenager@kloudtek.com>
2014-12-12 23:22:31 +0000

    In progress

---
+++ core/src/main/java/com/kloudtek/kryptotek/keystore/KeyStore.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2014 Kloudtek Ltd
+ */
+
+package com.kloudtek.kryptotek.keystore;
+
+import java.security.InvalidKeyException;
+
+/**
+ * Created by yannick on 22/11/2014.
+ */
+public interface KeyStore {
+    KeyStoreAccessToken getAccessToken(KeyStoreCredential credential) throws CredentialInvalidException, KeyStoreAccessException;
+
+    <X extends com.kloudtek.kryptotek.Key> com.kloudtek.kryptotek.Key getKey(Class<X> keyClass, String keyLabel, KeyStoreAccessToken keyStoreAccessToken) throws KeyNotFoundException, KeyStoreAccessException, InvalidKeyException;
+
+    com.kloudtek.kryptotek.Key getKey(String keyLabel, KeyStoreAccessToken keyStoreAccessToken) throws KeyNotFoundException, KeyStoreAccessException, InvalidKeyException;
+
+    com.kloudtek.kryptotek.Key getKey(String keyLabel) throws KeyNotFoundException, KeyStoreAccessException, InvalidKeyException;
+
+    void importKey(String label, com.kloudtek.kryptotek.Key key, KeyStoreAccessToken keyStoreAccessToken) throws KeyStoreAccessException;
+
+    void importKey(String label, com.kloudtek.kryptotek.Key key) throws KeyStoreAccessException;
+}
