5cc535ac7ec1dbeebd1f5c559ceba68e91eebafb
Yannick Menager <yannick.menager@mulesoft.com>
2017-06-22 22:25:35 -0700

    In progress

---
+++ rest/server/springsecurity/src/main/java/com/kloudtek/kryptotek/rest/RestSpringAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.kloudtek.kryptotek.rest;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by yannick on 6/21/17.
+ */
+public class RestSpringAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+            String method = method;
+            RESTRequestSigner restRequestSigner = new RESTRequestSigner(method, path.toString(), nonce, timestampStr, identity);
+
+        }
+    }
+}
